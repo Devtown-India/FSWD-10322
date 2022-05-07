03fadafa18ed38d42491f0b31793e778a05251d3
kartik18g <gupta.kartik18kg@gmail.com>
2022-05-07 19:58:01 +0530

    window object

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const arr = [1,2,3] 
+
+const obj = {
+    name:"C class",
+    brand: "Mercedes",
+    model:2022,
+    log:()=>{console.log('this is mercedes')}
+}
+
+console.log(window)
+
+
+console.log(window.innerHeight)
+console.log(window.innerWidth)
+
+// const name = window.prompt("What is your name ?")
+
+window.open("https://youtube.com","_blank")
+console.log(window.location)
