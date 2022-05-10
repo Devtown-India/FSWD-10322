b9bb211079624e58567868b38eb14bc6741ffa45
kartik18g <gupta.kartik18kg@gmail.com>
2022-05-10 19:12:04 +0530

    change

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 
-const arr = [1,2,3] 
+// const arr = [1,2,3] 
 
-const obj = {
-    name:"C class",
-    brand: "Mercedes",
-    model:2022,
-    log:()=>{console.log('this is mercedes')}
-}
+// const Car = function(name,brand,model){
+//     this.name = name
+//     this.brand = brand
+//     this.model = model
 
-console.log(window)
+//     return "this"
+// }
+
+// const car1 =  new Car('C 200D','Mercedes',2022)
+// // const car2 = new Car('M2 Comp','BMW',2022)
+
+// console.log(car1)
+// console.log(car2)
+
+// const obj = {
+//     name:"C class",
+//     brand: "Mercedes",
+//     model:2022,
+//     log:function(){
+//         console.log(this.name)
+//     }
+// }
+
+// console.log(this)
+// obj.log()
+
+const age = 10
+
+console.log(typeof age)
+
+const age_ = new Number(10)
+
+console.log(typeof age_)
 
 
-console.log(window.innerHeight)
-console.log(window.innerWidth)
-
-// const name = window.prompt("What is your name ?")
-
-window.open("https://youtube.com","_blank")
-console.log(window.location)
