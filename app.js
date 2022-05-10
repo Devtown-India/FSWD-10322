007cf426e37664ba0de4d35cd556e06772d96021
kartik18g <gupta.kartik18kg@gmail.com>
2022-05-10 19:37:52 +0530

    lcoal storage web api

--- app.js
+++ app.js
@@ -1,38 +1,5 @@
+// Web API's
 
-// const arr = [1,2,3] 
+// localStorage.setItem('model',"C Class")
 
-// const Car = function(name,brand,model){
-//     this.name = name
-//     this.brand = brand
-//     this.model = model
-
-//     return "this"
-// }
-
-// const car1 =  new Car('C 200D','Mercedes',2022)
-// // const car2 = new Car('M2 Comp','BMW',2022)
-
-// console.log(car1)
-// console.log(car2)
-
-// const obj = {
-//     name:"C class",
-//     brand: "Mercedes",
-//     model:2022,
-//     log:function(){
-//         console.log(this.name)
-//     }
-// }
-
-// console.log(this)
-// obj.log()
-
-const age = 10
-
-console.log(typeof age)
-
-const age_ = new Number(10)
-
-console.log(typeof age_)
-
-
+localStorage.removeItem('brand')
