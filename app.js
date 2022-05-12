377d5cb3a0fdac5c356c122d0398e88835876043
kartik18g <gupta.kartik18kg@gmail.com>
2022-05-12 19:40:33 +0530

    selectors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // console.log(document)
 
-const ele = document.getElementById('car')
+// const ele = document.getElementById('car')
+// const ele = document.querySelectorAll('.item')
+// console.log(ele)
 // ele.style='background:dodgerblue;font-size:40px'
 // ele.classList.a
-const arr = [1,2,3]
+// const arr = [1,2,3]
 
 // console.log(arr)
-ele.classList.add("dark")
-ele.classList.remove('something')
+// ele.classList.add("dark")
+// ele.classList.remove('something')
 
-console.log(ele.classList)
+// console.log(ele.classList)
