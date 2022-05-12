6533307a0881462a313aa6c9182c88bb1863667e
kartik18g <gupta.kartik18kg@gmail.com>
2022-05-12 20:19:18 +0530

    timer

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const counter = document.querySelector('h1#counter')
+const counter = document.querySelector("h1#counter");
 
-function increment(){
-    let val = counter.innerHTML
-    val++
-    counter.innerHTML=val
+const interval = function () {
+  let val = counter.innerHTML;
+  val++;
+  counter.innerHTML = val;
+};
+
+let intervalId = null;
+
+function start() {
+  if (intervalId) return window.alert("Timer is already running");
+  intervalId = setInterval(interval, 1000);
 }
 
-
-function decrement(){
-    let val = counter.innerHTML
-    if(val==0) return window.alert("Value is 0")
-    val--
-    counter.innerHTML=val
+function stop() {
+  if (!intervalId) return window.alert("NO timer to stop");
+  clearInterval(intervalId);
+  intervalId = null;
 }
 
+function reset() {
+  if (intervalId) {
+    stop();
+  }
+  counter.innerHTML = 0;
+}
+
+console.log(new Date().toLocaleTimeString())
