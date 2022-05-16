6b949456e0a61710e4852225b91052b5182113fd
kartik18g <gupta.kartik18kg@gmail.com>
2022-05-16 20:20:10 +0530

    array methods

--- app.js
+++ app.js
@@ -1,29 +1,62 @@
-const counter = document.querySelector("h1#counter");
+const arr = [1,2,3,4,5,6]
 
-const interval = function () {
-  let val = counter.innerHTML;
-  val++;
-  counter.innerHTML = val;
-};
-
-let intervalId = null;
-
-function start() {
-  if (intervalId) return window.alert("Timer is already running");
-  intervalId = setInterval(interval, 1000);
+const callback = (value,index)=>{
+  if(value==99) return true
 }
 
-function stop() {
-  if (!intervalId) return window.alert("NO timer to stop");
-  clearInterval(intervalId);
-  intervalId = null;
-}
+ const a = ()=>{}
 
-function reset() {
-  if (intervalId) {
-    stop();
-  }
-  counter.innerHTML = 0;
-}
+const isPresent = arr.find(callback)
+console.log(isPresent)
 
-console.log(new Date().toLocaleTimeString())
+// const newArr = arr.filter((value,index) => value%2===0?true:false)
+// console.log(newArr)
+
+// const num = 10;
+
+// const result = num%2===0 ? "even": "odd"
+// console.log(result)
+
+// if(num%2==0) console.log("even")
+// else console.log("false")
+
+
+
+// const Car = (brand,name)=>{
+//   console.log(this)
+//   this.name=name;
+//   this.brand=brand;
+// }
+
+// const a = new Car("Mercedes","C300D")
+// console.log(a)
+
+// sum(1,5)
+// // greet()
+
+
+// function greet(){
+//   console.log('hi')
+// }
+
+
+// const sum = function (a,b){
+//   console.log(a+b)
+// }
+
+
+// for(let i =0;i<arr.length;i++){
+//   console.log(arr[i])
+// }
+
+// let i = 0;
+
+// while(i<arr.length){
+//   console.log(arr[i])
+//   i++;
+// }
+
+// do {
+//   console.log(arr[i])
+//   i++
+// } while (i<arr.length-1);
