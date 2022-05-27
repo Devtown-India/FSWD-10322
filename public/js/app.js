7d74a290219aa8963e6599bdf1070e17a45bacc5
kartik18g <gupta.kartik18kg@gmail.com>
2022-05-27 20:15:30 +0530

    async await

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,16 @@ const endpoint = "https://jsonplaceholder.typicode.com/todos"
 
 
 const getData =  async ()=>{
-    const response = await new Promise(resolve=>{
-        setTimeout(()=>{
-            resolve()
-        },5000)
-    })
-    // console.log(response)
-    // console.log('hi')
-    // await
-    for(let i =0;i<10000;i++)console.log("hi")
+
+    try {
+        const response = await fetch(endpoint)
+        const jsonData = await response.json()
+        console.log(jsonData)
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 getData()
-console.log('something')
+// console.log('2')
