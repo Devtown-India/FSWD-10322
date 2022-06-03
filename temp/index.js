88e6b38d968c81ec133653b9bc8650ebe6c3ab35
kartik18g <gupta.kartik18kg@gmail.com>
2022-06-03 19:46:38 +0530

    path

---
+++ temp/index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+// console.log(path.resolve())
+
+// console.log(__dirname)
+
+// console.log(path.dirname('../utils.js'))
+// console.log(path.extname('../utils.js'))
+
+// console.log(path.relative('index.js','/Users/kartikgupta/Desktop/FSWD-10322/temp2/something.html'))
+
+console.log(path.join(path.resolve(),'/temp/index.js'))
+// console.log(path.)
+// console.log(path.)
+
+
+
+
+// './index.js' -> 'Users/kartik/somemthing/somthing/temp/indexjs'
