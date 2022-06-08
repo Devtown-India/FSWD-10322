403e738df082d0f8a748de85729a0b1a72634d71
kartik18g <gupta.kartik18kg@gmail.com>
2022-06-08 19:22:20 +0530

    express server

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('response')
+    res.send('home page')
 })
+
+app.get('/about',(req,res)=>{
+    res.send('about page')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('contact page')
+})
+
 
 app.listen(8080,()=>{
     console.log(`server started at Port: ${8080}`)
