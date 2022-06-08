41cca16e09551a573c0f69f514d86603380de660
kartik18g <gupta.kartik18kg@gmail.com>
2022-06-08 19:18:07 +0530

    express server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log('hi')
-console.log("something")
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('response')
+})
+
+app.listen(8080,()=>{
+    console.log(`server started at Port: ${8080}`)
+})
