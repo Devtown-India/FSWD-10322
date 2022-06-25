48ad6dd49853c8804fffbcde6ab342d9821d8a7d
kartik18g <gupta.kartik18kg@gmail.com>
2022-06-25 19:43:42 +0530

    changes

--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,28 @@ const router = express.Router();
 const { isAuthorised } = require("../middlewares/index");
 const { User, Note } = require("../models/index");
 
+router.get("/", isAuthorised, async (req, res) => {
+  try {
+    const userId = req.user;
+    const notes = await Note.find({author:userId});
+
+    const data = {
+      success: true,
+      data: { notes },
+      message: "Notes fetched successfully",
+    };
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    const data = {
+      success: false,
+      data: { notes: null },
+      message: error.message,
+    };
+    res.status(400).json(data);
+  }
+});
+
 router.post("/add", isAuthorised, async (req, res) => {
   try {
     const userId = req.user;
@@ -29,6 +51,45 @@ router.post("/add", isAuthorised, async (req, res) => {
       success: true,
       data: { note },
       message: "Note added successfully",
+    };
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    const data = {
+      success: false,
+      data: { note: null },
+      message: error.message,
+    };
+    res.status(400).json(data);
+  }
+});
+
+router.delete("/delete/:id", isAuthorised, async (req, res) => {
+  try {
+    const userId = req.user;
+    const {id:noteId} = req.params;
+    // find that note
+    const note = await Note.findById(noteId);
+    // check if the user is the author of the note
+    if(note.author.toString() !== userId){
+        const data = {
+            success: false,
+            data: { note: null },
+            message: "You are not authorized to delete this note",
+        };
+        return res.status(400).json(data);
+    }
+
+    await Note.findByIdAndDelete(noteId);
+    // ! delete the reference from the user
+    await User.findByIdAndUpdate(userId,{
+        $pull:{notes:noteId}
+    })
+  
+    const data = {
+      success: true,
+      data: { note },
+      message: "Note deleted successfully",
     };
     res.status(200).json(data);
   } catch (error) {
