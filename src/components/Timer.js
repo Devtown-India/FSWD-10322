53b5038dc8a2a54308ee6b8cc3da61d1010dca71
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-17 17:57:27 +0530

    timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,26 +3,45 @@ import { useState } from "react";
 
 const Timer = () => {
   const [time, setTime] = useState(0);
-
-  
+  const [timer,setTimer] = useState(null);
 
   useEffect(() => {
-
-    const timer = setInterval(() => {
-    setTime((prev) => prev + 1);
-  }, 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
+   return ()=>{
+     clearInterval(timer)
+   }
   },[]);
+
+  const startTimer = ()=>{
+     if(timer!=null) return window.alert("Timer is already running");
+     const t = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+    setTimer(t)
+  }
+
+  const handlePause = ()=>{
+    if(timer==null) return window.alert("Timer is not running")
+    clearInterval(timer)
+    setTimer(null)
+  }
+
+  const handleReset = ()=>{
+    if(timer==null){
+      setTime(0)
+      return
+    }
+    clearInterval(timer)
+    setTimer(null)
+    setTime(0)
+  }
+
 
   return (
     <div>
       <h1>{time}</h1>
-      <button>start</button>
-      <button>pause</button>
-      <button>reset</button>
+      <button onClick={startTimer} >start</button>
+      <button onClick={handlePause}>pause</button>
+      <button onClick={handleReset}  >reset</button>
     </div>
   );
 };
