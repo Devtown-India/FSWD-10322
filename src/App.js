5e6e24a5c90c029fe70b24d8ba5ced2c63509222
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-17 18:36:52 +0530

    change

--- src/App.js
+++ src/App.js
@@ -3,35 +3,35 @@ import { useEffect, useState } from "react";
 import Click from "./components/Click";
 import Home from "./components/Home";
 import Timer from "./components/Timer";
+import axios from 'axios';
 
 const App = () => {
-  // let count = 0;
-  const [display, setDisplay] = useState("");
+
+  const [todos,settodos] = useState([]);
+
+  const getData = async ()=>{
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+      const data = response.data
+      settodos(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
 
   return (
     <div>
-      <nav>
-        <ul>
-          <li
-            onClick={() => setDisplay("move")}
-          >
-            Move
-          </li>
-          <li onClick={() => setDisplay("click")}>Click</li>
-          <li onClick={() => setDisplay((prev) => "home")}>Home</li>
-        </ul>
-      </nav>
-      <section>
-        {display === "home" ? (
-          <Home />
-        ) : display === "click" ? (
-          <Click />
-        ) : display === "move" ? (
-          <Timer />
-        ) : (
-          "select a component to render"
-        )}
-      </section>
+      <ol>
+        {
+          todos.map(todo=>(
+            <li key={todo.id} > {todo.title}</li>
+          ))
+        }
+      </ol>
     </div>
   );
 };
