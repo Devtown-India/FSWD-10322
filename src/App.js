50d634418ea4aae18c96ae1cd6908660c1a35896
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-21 19:48:03 +0530

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import { Route, Routes } from "react-router";
-import { Link } from "react-router-dom";
-import About from "./components/About";
-import Home from "./components/Home";
-import Product from "./components/Product";
-import Todos from "./components/Todos";
-import Users from "./components/Users";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+import Recipe from "./components/Recipe";
 
 const App = () => {
-  console.log(window.location.pathname);
-
   return ( 
-    <div>
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/users">Users</Link></li>
-        <li><Link to="/todos">Todos</Link></li>
-        {/* <li><Link to="/todos">Produc</Link></li> */}
-      </ul>
-    </nav>
-      <Routes>
-        <Route path="/" element={<Home/>}  />
-        <Route path="/about" element={<About/>}  />
-        <Route path="/users" element={<Users/>}  />
-        <Route path="/todos" element={<Todos/>}  />
-        <Route path="/product/:phone" element={<Product/>}  />
-      </Routes>
-    </div>
+    <>
+    <Navbar/>
+     <div style={{height:"80vh"}} className="container my-2">
+      <Recipe/>
+     </div>
+    <Footer/>
+    </>
    );
 }
  
---
+++ src/components/Recipe.js
@@ -0,0 +1,11 @@
+import Search from "./Search";
+
+const Recipe = () => {
+  return (
+    <div className="">
+        <Search/>
+    </div>
+  );
+};
+
+export default Recipe;
