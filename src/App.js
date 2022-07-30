254fece78b52691ef2d5369d0a81bfe9d13f301a
kartik18g <gupta.kartik18kg@gmail.com>
2022-07-30 18:58:43 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { useSelector } from "react-redux";
-import List from "./components/List";
+import { Route, Routes } from "react-router";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Notes from "./components/notes/Notes";
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
 
 const App = () => {
 
   return (
     <div>
-      <List/>
+      <Navbar/>
+      <Routes>
+        <Route path="/login" element={<Login/>}  />
+        <Route path="/signup" element={<Signup/>}  />
+        <Route path="/notes" element={<Notes/>}  />
+      </Routes>
+      <Footer/>
     </div>
+
   );
 };
 
